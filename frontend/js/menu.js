96a1b193267884cc6da5b459095d27009a79597f
Jameel <sbijjj1@gmail.com>
2024-11-09 01:34:40 +0800

    fixed issues with the add to cart

--- frontend/js/menu.js
+++ frontend/js/menu.js
@@ -147,9 +147,13 @@ async function addToCart(itemId, itemName, itemPrice) {
         // Update cart count in the UI immediately
         incrementCartCount();
 
+        console.log('incremented cart count');
+
         // Optionally, fetch the updated cart data to ensure accuracy
         const cartData = await fetchCartData(userId);
         const cartCount = cartData.items.reduce((total, item) => total + item.quantity, 0);
+
+        console.log('cart count from fetched data from backend:', cartCount);
         updateCartCount(cartCount);
 
     } catch (error) {
